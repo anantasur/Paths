2130338a065cd6b76efe5932bfa41b3df9c91c4b
Ananthu R V <ananthurv666@hotmail.com>
2015-03-02 15:22:12 +0530

    method to find place exists is added

--- Paths.java
+++ Paths.java
@@ -2,13 +2,22 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Paths{
-	private Map<String, String> map = new HashMap<String, String>();
+	private Map<String, String> routes = new HashMap<String, String>();
 	
 	public void insertPath(String start,String destination) {
-		map.put(new String(start), new String(destination));
+		routes.put(new String(start), new String(destination));
 	}
 
+	public boolean isPresent(String place) {
+		return routes.containsKey(place);
+	}
+	
 	public boolean pathExists(String start,String destination) {
-		return destination.equals(map.get(start));
-	} 
+		return destination.equals(routes.get(start));
+	}
+	
+	// public static void main(String[] args) {
+		
+	// }
 }
+
--- PathsTest.java
+++ PathsTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import java.util.Map;
 import java.util.HashMap;
 
-public class PathsTest {
+public class PathsTest {	
 
 	@Test
 	public void there_is_no_path_between_Bangalore_Singapore(){
@@ -17,5 +17,19 @@ public class PathsTest {
 	public void there_is_path_between_singapore_Seol(){
 		Paths p = new Paths();
 		assertTrue(!p.pathExists("Singapore","Seoul"));
+	}
+
+	@Test
+	public void there_is_a_source_known_as_Bangalore(){
+		Paths p = new Paths();
+		p.insertPath("Bangalore","Singapore");
+		assertTrue(p.isPresent("Bangalore"));
+	}
+
+	@Test
+	public void there_is_a_source_known_as_Chennai(){
+		Paths p = new Paths();
+		p.insertPath("Bangalore","Singapore");
+		assertTrue(!p.isPresent("Chennai"));
 	}
 }
