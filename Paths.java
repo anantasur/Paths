69bb54c75d8708897a99b45a14e5cb80d415ae07
Ananthu R V <ananthurv666@hotmail.com>
2015-03-02 14:06:52 +0530

    tests written to check path exists or not

--- Paths.java
+++ Paths.java
@@ -1,3 +1,14 @@
-public class Paths {
+import java.util.Map;
+import java.util.HashMap;
+
+public class Paths{
+	private Map<String, String> map = new HashMap<String, String>();
 	
+	public void insertPath(String start,String destination) {
+		map.put(new String(start), new String(destination));
+	}
+
+	public boolean pathExists(String start,String destination) {
+		return destination.equals(map.get(start));
+	} 
 }
--- PathsTest.java
+++ PathsTest.java
@@ -1,7 +1,21 @@
 import org.junit.Test;
 import java.util.*;
 import static org.junit.Assert.*;
+import java.util.Map;
+import java.util.HashMap;
 
 public class PathsTest {
-	
+
+	@Test
+	public void there_is_no_path_between_Bangalore_Singapore(){
+		Paths p = new Paths();
+		p.insertPath("Bangalore","Singapore");
+		assertTrue(p.pathExists("Bangalore","Singapore"));
+	}
+
+	@Test
+	public void there_is_path_between_singapore_Seol(){
+		Paths p = new Paths();
+		assertTrue(!p.pathExists("Singapore","Seoul"));
+	}
 }
