f4bf600016e972169e23697a19b31e358cba21eb
Ananthu R V <ananthurv666@hotmail.com>
2015-03-02 17:10:44 +0530

    versioin one comleted with new data base

--- Paths.java
+++ Paths.java
@@ -1,19 +1,33 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Paths{
-	private Map<String, String> routes = new HashMap<String, String>();
-	
-	public void insertPath(String start,String destination) {
-		routes.put(new String(start), new String(destination));
-	}
+	static Map<String,List<String>> routes = new HashMap<String,List<String>>();
+	static {
+		List<String> bangalore = new ArrayList<String>();
+		bangalore.add("Singapore");
+		List<String> singapore = new ArrayList<String>();
+		singapore.add("Seoul");
+		singapore.add("Dubai");
+		List<String> seoul = new ArrayList<String>();
+		seoul.add("Beijing");
+		List<String> beijing = new ArrayList<String>();
+		beijing.add("Tokyo");
 
+		routes.put("Bangalore",bangalore);
+		routes.put("Singapore",singapore);
+		routes.put("Seoul",seoul);
+		routes.put("Beijing",beijing);
+	}
+	
 	public boolean isPresent(String place) {
 		return routes.containsKey(place);
 	}
 	
-	public boolean pathExists(String start,String destination) {
-		return destination.equals(routes.get(start));
+	public boolean pathExists(String source,String destination) {
+		return routes.get(source).contains(destination);
 	}
 	
 	public static void main(String[] args) {
@@ -22,12 +36,6 @@ public class Paths{
 		String destination = new String(args[1]);
 
 		Paths p = new Paths();
-		p.insertPath("Bangalore","Singapore");
-		p.insertPath("Singapore","Seoul");
-		p.insertPath("Singapore","Dubai");
-		p.insertPath("Seoul","Beijing");
-		p.insertPath("Beijing","Tokyo");
-
 		if(!p.isPresent(source)){
 			System.out.println("No city named \""+source+"\" in database");
 			return;
--- PathsTest.java
+++ PathsTest.java
@@ -9,27 +9,24 @@ public class PathsTest {
 	@Test
 	public void there_is_no_path_between_Bangalore_Singapore(){
 		Paths p = new Paths();
-		p.insertPath("Bangalore","Singapore");
 		assertTrue(p.pathExists("Bangalore","Singapore"));
 	}
 
 	@Test
-	public void there_is_path_between_singapore_Seol(){
+	public void there_is_path_between_singapore_Beijing(){
 		Paths p = new Paths();
-		assertTrue(!p.pathExists("Singapore","Seoul"));
+		assertTrue(!p.pathExists("Singapore","Beijing"));
 	}
 
 	@Test
 	public void there_is_a_source_known_as_Bangalore(){
 		Paths p = new Paths();
-		p.insertPath("Bangalore","Singapore");
 		assertTrue(p.isPresent("Bangalore"));
 	}
 
 	@Test
 	public void there_is_a_source_known_as_Chennai(){
 		Paths p = new Paths();
-		p.insertPath("Bangalore","Singapore");
 		assertTrue(!p.isPresent("Chennai"));
 	}
 }
