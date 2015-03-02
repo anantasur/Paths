3dcf1df5834f7e0697833779b8ae3125b7d2e92d
Ananthu R V <ananthurv666@hotmail.com>
2015-03-02 15:33:50 +0530

    version 1 completed with cli

--- Paths.java
+++ Paths.java
@@ -16,8 +16,27 @@ public class Paths{
 		return destination.equals(routes.get(start));
 	}
 	
-	// public static void main(String[] args) {
-		
-	// }
+	public static void main(String[] args) {
+
+		String source = new String(args[0]);
+		String destination = new String(args[1]);
+
+		Paths p = new Paths();
+		p.insertPath("Bangalore","Singapore");
+		p.insertPath("Singapore","Seoul");
+		p.insertPath("Singapore","Dubai");
+		p.insertPath("Seoul","Beijing");
+		p.insertPath("Beijing","Tokyo");
+
+		if(!p.isPresent(source)){
+			System.out.println("No city named \""+source+"\" in database");
+			return;
+		}
+		if(p.pathExists(source, destination)){
+			System.out.println("True");
+		}else{
+			System.out.println("No city named \""+destination+"\" in database");
+		}
+	}
 }
 
