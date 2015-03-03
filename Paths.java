33191dd613d489f91d34115329960df44f2f74b4
Ananthu R V <ananthurv666@hotmail.com>
2015-03-03 16:57:53 +0530

    version 3 implemented

--- Paths.java
+++ Paths.java
@@ -1,50 +1,109 @@
 import java.util.Map;
-import java.util.HashMap;
+import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class Paths{
-	static Map<String,List<String>> routes = new HashMap<String,List<String>>();
-	static {
-		List<String> bangalore = new ArrayList<String>();
-		bangalore.add("Singapore");
-		List<String> singapore = new ArrayList<String>();
-		singapore.add("Seoul");
-		singapore.add("Dubai");
-		List<String> seoul = new ArrayList<String>();
-		seoul.add("Beijing");
-		List<String> beijing = new ArrayList<String>();
-		beijing.add("Tokyo");
+public class Paths {
+	static Map<String, List<String>> routes = new HashMap<String, List<String>>();
+	Queue path = new LinkedList();
+	static{
+			List<String> bangalore = new ArrayList<String>();
+			bangalore.add("Singapore");
 
-		routes.put("Bangalore",bangalore);
-		routes.put("Singapore",singapore);
-		routes.put("Seoul",seoul);
-		routes.put("Beijing",beijing);
+			List<String> singapore = new ArrayList<String>();
+			singapore.add("Seoul");
+			singapore.add("Dubai");
+			singapore.add("Bangalore");
+
+			List<String> seoul = new ArrayList<String>();
+			seoul.add("Singapore");
+			seoul.add("Beijing");
+
+			List<String> beijing = new ArrayList<String>();
+			beijing.add("Seoul");
+			beijing.add("Tokyo");
+
+			List<String> dubai = new ArrayList<String>();
+			dubai.add("Singapore");
+
+			List<String> tokyo = new ArrayList<String>();
+			tokyo.add("Beijing");
+
+			routes.put("Bangalore", bangalore);
+			routes.put("Singapore", singapore);
+			routes.put("Seoul", seoul);
+			routes.put("Beijing", beijing);
+			routes.put("Dubai", dubai);
+			routes.put("Tokyo", tokyo);
 	}
-	
-	public boolean isPresent(String place) {
-		return routes.containsKey(place);
+
+	public boolean isPresent(String city){
+		Set<String> sourceStations = routes.keySet();
+		if(sourceStations.contains(city))
+			return true;
+		else{
+			for(String source : sourceStations){
+				List<String> destinations = routes.get(source) ;
+				if(destinations.contains(city))
+					return true;
+			}
+		}
+		return false;
 	}
-	
-	public boolean pathExists(String source,String destination) {
-		return routes.get(source).contains(destination);
+
+	public boolean hasPath(String source, String destination){
+		path.add(source);
+		return (getFullPath(source,destination) == 1) ? true : false;
 	}
-	
+
+	public int getFullPath(String source, String destination){
+		if(routes.get(source) == null) return 0;
+		if(routes.get(source).contains(destination)){
+			path.add(destination);
+			return 1;
+		}
+		if(!routes.get(source).contains(destination)){			
+			int size = routes.get(source).size();
+			for(int i = 0; i < size; i++){
+				if(!path.contains(routes.get(source).get(i))){
+					path.add(routes.get(source).get(i));
+					return getFullPath(routes.get(source).get(i),destination);
+				}
+			}
+		}
+		return 0;
+	}
+
+
+	public String getPath(String source, String destination){
+		boolean hasPath = hasPath(source,destination);
+		int size = path.size();
+		String fullPath = "";
+		for(int i = 0; i < size; i++){
+			if(i>0)
+				fullPath += "->"+path.poll();
+			else
+				fullPath += ""+path.poll();
+		}
+		return fullPath;
+	}
+
 	public static void main(String[] args) {
-
-		String source = new String(args[0]);
-		String destination = new String(args[1]);
-
-		Paths p = new Paths();
-		if(!p.isPresent(source)){
-			System.out.println("No city named \""+source+"\" in database");
+		String source = args[0];
+		String destination = args[1];
+		Paths paths = new Paths();
+		if(!paths.isPresent(source)){
+			System.out.println("No city named '"+source+"' in database");
 			return;
 		}
-		if(p.pathExists(source, destination)){
-			System.out.println("True");
-		}else{
-			System.out.println("No city named \""+destination+"\" in database");
+		if(!paths.isPresent(destination)){
+			System.out.println("No city named '"+destination+"' in database");
+			return;
 		}
+		else
+			System.out.println(paths.getPath(source,destination));
 	}
 }
-
--- PathsTest.java
+++ PathsTest.java
@@ -1,32 +1,70 @@
 import org.junit.Test;
-import java.util.*;
 import static org.junit.Assert.*;
-import java.util.Map;
-import java.util.HashMap;
 
-public class PathsTest {	
 
+public class PathsTest {
+	
 	@Test
-	public void there_is_no_path_between_Bangalore_Singapore(){
-		Paths p = new Paths();
-		assertTrue(p.pathExists("Bangalore","Singapore"));
+	public void isCityPresent_checks_whether_city_is_prsent (){
+		Paths paths = new Paths();
+		assertFalse(paths.isPresent("Chennai"));
+		assertTrue(paths.isPresent("Dubai"));
+		assertTrue(paths.isPresent("Tokyo"));
+		assertTrue(paths.isPresent("Singapore"));
+		assertTrue(paths.isPresent("Beijing"));
 	}
 
 	@Test
-	public void there_is_path_between_singapore_Beijing(){
-		Paths p = new Paths();
-		assertTrue(!p.pathExists("Singapore","Beijing"));
+	public void hasPath_returns_true_if_there_is_direct_flight_between_Bangalore_and_Singapore (){
+		Paths paths = new Paths();
+		assertTrue(paths.hasPath("Bangalore", "Singapore"));
+		assertTrue(paths.hasPath("Singapore", "Bangalore"));
 	}
 
 	@Test
-	public void there_is_a_source_known_as_Bangalore(){
-		Paths p = new Paths();
-		assertTrue(p.isPresent("Bangalore"));
+	public void hasPath_returns_true_if_there_is_direct_flight_between_Seoul_and_Beijing (){
+		Paths paths = new Paths();
+		assertTrue(paths.hasPath("Seoul", "Beijing"));
+		assertTrue(paths.hasPath("Beijing", "Seoul"));
 	}
 
 	@Test
-	public void there_is_a_source_known_as_Chennai(){
-		Paths p = new Paths();
-		assertTrue(!p.isPresent("Chennai"));
+	public void hasPath_returns_true_if_there_is_direct_flight_between_Beijing_and_Tokyo (){
+		Paths paths = new Paths();
+		assertTrue(paths.hasPath("Beijing", "Tokyo"));
+		assertTrue(paths.hasPath("Tokyo", "Beijing"));
+	}
+	
+	@Test
+	public void hasPath_returns_true_if_there_is_direct_flight_between_Singapore_and_Dubai (){
+		Paths paths = new Paths();
+		assertTrue(paths.hasPath("Singapore", "Dubai"));
+		assertTrue(paths.hasPath("Dubai", "Singapore"));
+	}
+
+	@Test
+	public void hasPath_returns_true_if_there_is_direct_flight_between_Singapore_and_Seoul (){
+		Paths paths = new Paths();
+		assertTrue(paths.hasPath("Singapore", "Seoul"));
+		assertTrue(paths.hasPath("Seoul", "Singapore"));
+	}
+
+	@Test
+	public void hasPath_returns_true_if_there_is_flight_between_Bangalore_and_Tokyo (){
+		Paths paths = new Paths();
+		assertTrue(paths.hasPath("Bangalore", "Tokyo"));
+	}
+
+	@Test
+	public void hasPath_returns_true_if_there_is_flight_between_Tokyo_and_Bangalore (){
+		Paths paths = new Paths();
+		assertTrue(paths.hasPath("Tokyo", "Bangalore"));
+	}
+
+	@Test
+	public void givePath_gives_full_path_between_two_places (){
+		Paths paths = new Paths();
+		assertEquals("Bangalore->Singapore", paths.getPath("Bangalore", "Singapore"));
+		assertEquals("Bangalore->Singapore->Seoul->Beijing->Tokyo", paths.getPath("Bangalore", "Tokyo"));
 	}
 }
